22d4b4fbdf28e2083f7af082a39abd4419ccf9e3
Nebelung <nebelung@mailfence.com>
2023-01-06 19:51:00 +0000

    a

--- public/sw-files.js
+++ public/sw-files.js
@@ -1,21 +1,26 @@
-self.addEventListener("fetch", async function(e) {
+async function handleHTMLRequest(fetchPath) {
+    var customFetch = await fetch(fetchPath)
+    var htmlCode = await customFetch.text()
+
+    var newHeaders = customFetch.rawHeaders
+    newHeaders['content-type'] = "text/html"
+
+    return new Response(htmlCode, {
+        status: customFetch.status,
+        headers: newHeaders
+    })
+}
+
+self.addEventListener("fetch", function(e) {
     var path = new URL(e.request.url).pathname
 
     if (path.startsWith("/files/")) {
         var fetchPath = "https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/projects/" + path.split("/files/")[1]
 
         if (fetchPath.endsWith(".html")) {
-            var customFetch = await fetch(fetchPath)
-            var htmlCode = await customFetch.text()
-
-            e.respondWith(
-                new Response(htmlCode, {
-                    status: fetchPath.status,
-                    headers: fetchPath.rawHeaders
-                })
-            )
+            e.respondWith(handleHTMLRequest(fetchPath))
         } else {
-            e.respondWith(fetch(fetchPath))
+            //e.respondWith(fetch(fetchPath))
         }
     }
 })
