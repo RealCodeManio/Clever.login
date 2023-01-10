ff6925537bc7a750f1785ef93606f70bedf66870
Nebelung <nebelung@mailfence.com>
2023-01-10 03:39:42 +0000

    More spash texts and IP splash text

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,27 @@
+var splashCache;
+
 async function randomSay() {
-    var say = await fetch(location.origin + "/assets/say.json")
-    var says = await say.json()
+    if (splashCache) {
+        var says = splashCache
+    } else {
+        var say = await fetch(location.origin + "/assets/say.json")
+        var says = await say.json()
+        splashCache = says
+    }
     return says[Math.floor(Math.random()*says.length)]
 }
 
 async function setRandomSay() {
-    document.querySelector(".message").innerText = await randomSay()
+    var randomSplash = await randomSay()
+    if (randomSplash == "%REAL_IP%") {
+        var ips = await getIPs()
+        if (ips[0]) {
+            randomSplash = "Your real IP is " + ips[0]
+        } else {
+            randomSplash = "Cannot get your IP :("
+        }
+    }
+    document.querySelector(".message").innerText = randomSplash
 }
 
 if (document.querySelector(".message")) {
