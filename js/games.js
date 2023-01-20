26d1e1a484343d1dbb95a42bd81cd15ca0652119
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2023-01-20 14:43:34 -0800

    Update games.js

--- js/games.js
+++ js/games.js
@@ -23,7 +23,7 @@ function searchGames(query) {
     }
 }
 
-(async () => {
+async function addGames() {
     var gamesElement = document.querySelector(".games")
 
     var gamesData = await fetch(location.origin + "/assets/games.json")
@@ -50,4 +50,4 @@ function searchGames(query) {
     }
 
     document.querySelector(".spinner").style.display = "none"
-})()
+}
