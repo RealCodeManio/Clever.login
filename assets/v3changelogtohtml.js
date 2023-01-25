49c47848e62209a76f5f3204e832e01a179b5071
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2023-01-24 16:32:06 -0800

    make a little script to turn the changelog to html

---
+++ assets/v3changelogtohtml.js
@@ -0,0 +1,33 @@
+//3kh0.github.io changelog txt file to html format
+//paste in console to generate html
+
+async function v3changelogtohtml() {
+var changelog = ""
+
+var changelogCdn = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/dump/changelog.txt")
+var changelogTxt = await changelogCdn.text()
+changelogTxt = changelogTxt.substring(changelogTxt.indexOf("------------------------------------------------------------") + 1)
+changelogTxt = changelogTxt.replace("Looks like you have reached the end!", "")
+changelogTxt = changelogTxt.trim()
+changelogTxt = changelogTxt.replaceAll("------------------------------------------------------------", "")
+changelogTxt = changelogTxt.replaceAll("-----------------------------------------------------------", "")
+
+var changelogList = changelogTxt.split("\n")
+changelogList = changelogList.filter(item => item)
+
+for (let item in changelogList) {
+if (changelogList[item].startsWith("+ ")) {
+    changelog = changelog + "<added>" + changelogList[item].replace("+ ", "") + "</added>\n"
+} else if (changelogList[item].startsWith("^ ")) {
+    changelog = changelog + "<changed>" + changelogList[item].replace("^ ", "") + "</changed>\n"
+} else if (changelogList[item].startsWith("- ")) {
+    changelog = changelog + "<removed>" + changelogList[item].replace("- ", "") + "</removed>\n"
+} else {
+    changelog = changelog + "<p>" + changelogList[item] + "</p>\n"
+}
+}
+    
+return changelog;
+}
+
+console.log(await v3changelogtohtml())
