7a0eefc0d1f924f8c0937cc44ab0bce018456a26
Echo <echo-the-coder@tuta.io>
2023-01-29 21:01:30 -0500

    Indent

--- assets/v3gamestojson.js
+++ assets/v3gamestojson.js
@@ -3,35 +3,35 @@
 //paste in console to generate json
 
 async function v3gamestojson() {
-var games = []
+  var games = [];
 
-var projectsCdn = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/projects.html")
-var projectsCode = await projectsCdn.text()
-projectsCode = projectsCode.split(`<div id="search" class="flex-container">`)[1]
-projectsCode = projectsCode.split(` </div>
+  var projectsCdn = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/projects.html");
+  var projectsCode = await projectsCdn.text();
+  projectsCode = projectsCode.split(`<div id="search" class="flex-container">`)[1];
+  projectsCode = projectsCode.split(` </div>
     <p style="font-size: 25px;" class="text-center">
       Cant find what you are looking for?<br />
       <span style="font-size: 18px;">It might be in the <a href="/dump/index.html">Miscellaneous section</a>!</span>
-    </p>`)[0]
-var parser = new DOMParser();
-var doc = parser.parseFromString(projectsCode, "text/html");
+    </p>`)[0];
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(projectsCode, "text/html");
 
-var gamesElements = doc.body.children
+  var gamesElements = doc.body.children;
 
-for (let game in gamesElements) {
-if (gamesElements[game] instanceof Element) {
-var name = gamesElements[game].querySelector(".game-title").innerText.trim().replaceAll(`\n             Hot`, "")
-var url = gamesElements[game].getAttribute("href").trim().replace("projects", "/files")
-var img = gamesElements[game].querySelector(".game-icon").getAttribute("src").trim().replace("projects", "/files")
-games.push({
-    name: name,
-    url: url,
-    img: img
-})
+  for (let game in gamesElements) {
+    if (gamesElements[game] instanceof Element) {
+      var name = gamesElements[game].querySelector(".game-title").innerText.trim().replaceAll(`\n             Hot`, "");
+      var url = gamesElements[game].getAttribute("href").trim().replace("projects", "/files");
+      var img = gamesElements[game].querySelector(".game-icon").getAttribute("src").trim().replace("projects", "/files");
+      games.push({
+        name: name,
+        url: url,
+        img: img,
+      });
+    }
+  }
+
+  return JSON.stringify(games, null, 4);
 }
-}
 
-return JSON.stringify(games, null, 4);
-}
-
-console.log(await v3gamestojson())
+console.log(await v3gamestojson());
--- assets/v3reviewstojson.js
+++ assets/v3reviewstojson.js
@@ -3,40 +3,40 @@
 //paste in console to generate json
 
 async function v3reviewstojson() {
-var reviews = []
-    
-var indexCdn = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/index.html")
-var indexCode = await indexCdn.text()
-indexCode = indexCode.split(`<div class="carousel-inner">`)[1]
-indexCode = indexCode.split(`</div>
+  var reviews = [];
 
-        <!-- Left and right controls -->`)[0]
-    
-var parser = new DOMParser();
-var doc = parser.parseFromString(indexCode, "text/html");
+  var indexCdn = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/index.html");
+  var indexCode = await indexCdn.text();
+  indexCode = indexCode.split(`<div class="carousel-inner">`)[1];
+  indexCode = indexCode.split(`</div>
 
-var reviewItems = doc.body.children
+        <!-- Left and right controls -->`)[0];
 
-for (let reviewItem in reviewItems) {
-if (reviewItems[reviewItem] instanceof Element) {
-var reviewItemReviews = reviewItems[reviewItem].children[0].children
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(indexCode, "text/html");
 
-for (let review in reviewItemReviews) {
-if (reviewItemReviews[review] instanceof Element) {
-var name = reviewItemReviews[review].querySelector(".card-body").children[0].innerText.trim()
-var img = reviewItemReviews[review].querySelector(".card-img-top").getAttribute("src").trim().replace("/img/", "/images/").replace("img/", "/images/")
-var reviewContent = reviewItemReviews[review].querySelector(".card-text").innerText.trim()
-reviews.push({
-    name: name,
-    img: img,
-    review: reviewContent
-})
+  var reviewItems = doc.body.children;
+
+  for (let reviewItem in reviewItems) {
+    if (reviewItems[reviewItem] instanceof Element) {
+      var reviewItemReviews = reviewItems[reviewItem].children[0].children;
+
+      for (let review in reviewItemReviews) {
+        if (reviewItemReviews[review] instanceof Element) {
+          var name = reviewItemReviews[review].querySelector(".card-body").children[0].innerText.trim();
+          var img = reviewItemReviews[review].querySelector(".card-img-top").getAttribute("src").trim().replace("/img/", "/images/").replace("img/", "/images/");
+          var reviewContent = reviewItemReviews[review].querySelector(".card-text").innerText.trim();
+          reviews.push({
+            name: name,
+            img: img,
+            review: reviewContent,
+          });
+        }
+      }
+    }
+  }
+
+  return JSON.stringify(reviews, null, 4);
 }
-}
-}
-}
 
-return JSON.stringify(reviews, null, 4);
-}
-
-console.log(await v3reviewstojson())
+console.log(await v3reviewstojson());
