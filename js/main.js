56a24f776f9b594635c4e6fe380af57dbebb466f
Echo <echo-the-coder@tuta.io>
2023-02-04 23:28:02 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,29 +24,24 @@ inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
       gtag('js', new Date());
       gtag('config', 'G-98DP5VKS42');`;
 document.head.append(gascript, inlinegascript);
-script("Injected script 1/5 (Google Tag Manager)");
-
-const tabCloak = document.createElement("script");
-tabCloak.setAttribute("src", "/js/tab_cloak.js");
-document.head.append(tabCloak);
-script("Injected script 2/5 (Tab Cloaking)");
+script("Injected script 1/4 (Google Tag Manager)");
 
 const arc = document.createElement("script");
 arc.setAttribute("async", "");
 arc.setAttribute("src", "https://arc.io/widget.min.js#eRPHFgiC");
 document.head.append(arc);
-script("Injected script 3/5 (Widget stuff)");
+script("Injected script 2/4 (Widget stuff)");
 
 const adblock = document.createElement("script");
 adblock.setAttribute("src", "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1");
 adblock.setAttribute("nonce", "yibq-w_TR5NOCRWsU-VL0Q");
 adblock.setAttribute("async", "");
 document.head.append(adblock);
-script("Injected script 4/5 (Ad stuff)");
+script("Injected script 3/4 (Ad stuff)");
 
 const ad = document.createElement("script");
 ad.setAttribute("async", "");
 ad.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5756835229788588");
 ad.setAttribute("crossorigin", "anonymous");
 document.head.append(ad);
-script("Injected script 3/5 (Ad stuff)");
+script("Injected script 4/4 (Ad stuff)");
