6cfd2c36df0a1a4944b6165dedaf553032ee9d1d
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2023-02-06 17:32:42 -0800

    Version should update faster now

--- js/about.js
+++ js/about.js
@@ -16,7 +16,7 @@
     } catch {}
   
     try {
-      var infoFetch = await fetch("https://cdn.jsdelivr.net/gh/3kh0/3kh0.github.io/info.json");
+      var infoFetch = await fetch("https://raw.githack.com/3kh0/3kh0.github.io/main/info.json");
       try {
         var infoResult = await infoFetch.json();
         if (infoResult.version) {
