eaa1e4212d84e32250bde5168d33e77031276197
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2023-02-07 19:14:33 -0800

    test

--- sw.js
+++ sw.js
@@ -25,6 +25,8 @@ return cdns[0];
 async function handleRequest(fetchPath) {
     var currentCDN = await getCDN(["https://raw.githack.com/3kh0/3kh0-assets/main", "https://d1wnfatapmxxni.cloudfront.net", "https://d38a7mob3guz4f.cloudfront.net", "https://cloudbase-labs.s3.amazonaws.com"])
 
+    console.log(currentCDN)
+    
     fetchPath = currentCDN + fetchPath
 
     if (!fetchPath.endsWith(".html")) {
