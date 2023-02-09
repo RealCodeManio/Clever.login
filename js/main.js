cba6f1ea3cfd7d116b1d473a7ba985cf284af0c9
Echo <echo-the-coder@tuta.io>
2023-02-08 20:54:25 -0500

    Fix 'inlinegascript' has already been declared
    Error: Uncaught SyntaxError: Identifier 'inlinegascript' has already been declared (at main.js:1:1)

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ function script(text) {
 const gogascript27 = document.createElement("script");
 gogascript27.setAttribute("async", "");
 gogascript27.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
-const inlinegascript = document.createElement("script");
-inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+const inlinegogascript843 = document.createElement("script");
+inlinegogascript843.innerHTML = `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-98DP5VKS42');`;
-document.head.append(gogascript27, inlinegascript);
+document.head.append(gogascript27, inlinegogascript843);
 script("Injected script 1/4 (Google Tag Manager)");
 
 const arc = document.createElement("script");
